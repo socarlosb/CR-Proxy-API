7b73d56f35a6b88b436fdeb8152cb9133e47c47c
Carlos <socarlosb@gmail.com>
2020-09-02 11:06:27 +0100

    first edition

---
+++ index.js
@@ -0,0 +1,29 @@
+require("dotenv").config();
+
+const fastify = require("fastify")({
+  logger: true,
+});
+
+const axios = require("axios").default;
+
+const proxyToVendor = async (proxyUrl) => {
+  try {
+    const response = await axios.get(
+      `${process.env.PROXY_ENDPOINT}${proxyUrl}`,
+      { headers: { Authorization: `Bearer ${process.env.PROXY_TOKEN}` } }
+    );
+    return response;
+  } catch (error) {
+    return error.response;
+  }
+};
+
+fastify.get("*", async (request, reply) => {
+  const { status, data } = await proxyToVendor(request.url);
+  return { status, data };
+});
+
+fastify.listen(process.env.PORT, (err, address) => {
+  if (err) throw err;
+  fastify.log.info(`server listening on ${address}`);
+});
